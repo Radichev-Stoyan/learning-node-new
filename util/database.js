3c914352b36827c192c0dadd2211215f8a5af5e3
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-10-21 10:31:50 +0100

    Working with SQL and Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+const password = require('../pass');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: '15112298Stoy@n'
+const sequelize = new Sequelize('node-complete', 'root', password, {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
